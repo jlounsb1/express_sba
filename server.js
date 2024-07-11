d43599351c6d70f5e26d5b7a3de681c8e9e38716
jlounsb1 <jefflounsbury@gmail.com>
2024-07-11 11:28:46 -0400

    added comments for clarity

--- server.js
+++ server.js
@@ -5,19 +5,25 @@ const error = require('./utilities/error');
 const bodyParser = require('body-parser');
 const pug = require('pug');
 const fs = require('fs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
 const logReq = function(req, res, next) {
     console.log('you did something')
     next();
 }
+//all my global variables including requirements and middleware logging function
+
+//middleware so bodyparser works
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+//loads stylesheet capabilities and logs simple response middleware
 app.use(logReq)
 app.use(express.static('styles'))
 
+//needed for pug to work
 app.set("views", "./views");
 app.set('view engine', 'pug');
 
+//basic home page loads pug index view
 app.get('/', (req, res) => {
     res.render(
         'index'
@@ -25,15 +31,17 @@ app.get('/', (req, res) => {
 })
 
 
-
+//error handling middleware that does a simple 404
 app.use((req, res, next) =>{
     next(error(404, 'You done goofed! This does not exist.'))
 })
 
+//error handling middleware that pulls status code
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({ error: err.message });
 })
+
 
 app.listen(port, ()=>{
     console.log(`server is listening at port ${port}`)
