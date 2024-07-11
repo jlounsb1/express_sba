7e1eb9f7c99096df0043f6efcb5ba6e8c4c48ed1
jlounsb1 <jefflounsbury@gmail.com>
2024-07-11 16:03:25 -0400

    added a view and a route to show a specfic build

--- routes/builds.js
+++ routes/builds.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router= express.Router();
 const builds = require('../data/builds')
-
+//route to see all the builds
 router
     .route('/')
     .get((req, res)=>{
@@ -13,5 +13,18 @@ router
             }
         )   
     })
-
+//route to see a specific build
+router
+    .route('/:id')
+    .get((req, res) =>{
+        let buildNumber = req.params.id
+        let build = builds[(buildNumber-1)];
+        console.log(build)
+        res.render(
+            'specificbuild',
+            {
+                build: build
+            }
+        )
+    })
 module.exports = router;
