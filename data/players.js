229d34e8dffe8628d6f9aea3f31bf7a5af73960a
jlounsb1 <jefflounsbury@gmail.com>
2024-07-12 09:28:26 -0400

    added a players file in data

---
+++ data/players.js
@@ -0,0 +1,18 @@
+//creating my data for players
+//data to include:
+//userId
+//username:
+//playtimeHrs:
+//AccountPublic
+//
+
+const players = [
+    {
+        username: 'Smerg',
+        userId: 0,
+        playTimeHrs: 5000,
+        AccountPublic: true
+    }
+]
+
+modules.export = players
